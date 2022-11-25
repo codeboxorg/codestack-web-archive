c933e25f4d57426d64042c1a9104903184a99566
김정윤 <kjyjh9930@gmail.com>
2022-11-25 23:12:47 +0900

    chore : tailwindcss 적용

--- pages/index.page.tsx
+++ pages/index.page.tsx
@@ -3,7 +3,12 @@ import style from './index.module.scss'
 const cx = classNames.bind(style)
 
 const Landing = () => {
-  return <div className={cx('box', 'colorBlue')}>Landing</div>
+  return (
+    <>
+      <div className={cx('box', 'colorBlue')}>Landing</div>
+      <div className="w-200 h-200 bg-red-500">Landing</div>
+    </>
+  )
 }
 
 export default Landing
---
+++ tailwind.config.js
@@ -0,0 +1,56 @@
+/** @type {import('tailwindcss').Config} */
+const plugin = require('tailwindcss/plugin')
+const forms = require('@tailwindcss/forms')
+
+const px0_50 = { ...Array.from(Array(51)).map((_, i) => `${i}px`) }
+const px0_100 = { ...Array.from(Array(101)).map((_, i) => `${i}px`) }
+const px0_300 = { ...Array.from(Array(301)).map((_, i) => `${i}px`) }
+const px0_1920 = { ...Array.from(Array(1921)).map((_, i) => `${i}px`) }
+
+const custom = plugin(({ addUtilities, addVariant }) => {
+  addUtilities({
+    '.flex-center': {
+      display: 'flex',
+      'align-items': 'center',
+      'justify-content': 'center',
+    },
+    '.keep-words': {
+      'word-break': 'keep-all',
+    },
+  })
+  addVariant('all', '& *')
+  addVariant('under', '& > *')
+})
+
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    fontFamily: {},
+    extend: {
+      borderWidth: px0_50,
+      width: px0_1920,
+      height: px0_1920,
+      minWidth: px0_1920,
+      maxWidth: px0_1920,
+      minHeight: px0_1920,
+      maxHeight: px0_1920,
+      fontSize: px0_100,
+      padding: px0_100,
+      margin: px0_100,
+      spacing: px0_300,
+    },
+  },
+  darkMode: false,
+  variants: {
+    extend: {
+      outline: ['focus'],
+    },
+  },
+  plugins: [forms, custom],
+  corePlugins: {
+    preflight: true, //false:tailwind css적용순서를 첫번째로 하지 않음
+  },
+}
