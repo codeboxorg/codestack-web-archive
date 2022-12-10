07965fa02f1895456368829b5d4d6557a4b3b3a9
김정윤 <kjyjh9930@gmail.com>
2022-12-10 15:37:07 +0900

    test : vercel 프로덕션 배포 환경에서의 api 테스트용도 코드 추가

--- pages/api/[...path].api.ts
+++ pages/api/[...path].api.ts
@@ -8,47 +8,46 @@ import { AxiosError } from 'axios'
 import { body } from 'express-validator'
 
 oauthHandler.get<NextRequest<GithubAuthorizationToAccessTokenRequest>>(
-  'api/',
   async (req, res) => {
-    res.status(200).json({ test: 'test' })
+    res.status(200).json({ test: 'test2' })
   }
 )
 
-oauthHandler.get<NextRequest<GithubAuthorizationToAccessTokenRequest>>(
-  'api/oauth/test',
-  async (req, res) => {
-    res.status(200).json({ test: 'test' })
-  }
-)
+// oauthHandler.get<NextRequest<GithubAuthorizationToAccessTokenRequest>>(
+//   'api/oauth/test',
+//   async (req, res) => {
+//     res.status(200).json({ test: 'test' })
+//   }
+// )
 
-oauthHandler.post<NextRequest<GithubAuthorizationToAccessTokenRequest>>(
-  'api/oauth/github',
-  initValidation([
-    body('code').notEmpty().withMessage('Authorization code is empty'),
-    body('client_id').notEmpty().withMessage('Client id is empty'),
-    body('client_secret').notEmpty().withMessage('Client secret is empty'),
-  ]),
-  async (req, res) => {
-    const { code, client_id, client_secret } = req.body
-    try {
-      const fromServerResponse =
-        await serverToServerAPI.oauthServerToServer.githubAuthorizationToAccessToken(
-          code,
-          client_id,
-          client_secret
-        )
-      const response: OAuthAuthorizationToAccessTokenRequestResponse = {
-        accessToken: fromServerResponse.access_token,
-      }
-      res.status(200).json(response)
-    } catch (err) {
-      if (err instanceof AxiosError) {
-        const status = err.response?.status
-        const data = err.response?.data
-        res.status(status ?? 500).json(data)
-      }
-    }
-  }
-)
+// oauthHandler.post<NextRequest<GithubAuthorizationToAccessTokenRequest>>(
+//   'api/oauth/github',
+//   initValidation([
+//     body('code').notEmpty().withMessage('Authorization code is empty'),
+//     body('client_id').notEmpty().withMessage('Client id is empty'),
+//     body('client_secret').notEmpty().withMessage('Client secret is empty'),
+//   ]),
+//   async (req, res) => {
+//     const { code, client_id, client_secret } = req.body
+//     try {
+//       const fromServerResponse =
+//         await serverToServerAPI.oauthServerToServer.githubAuthorizationToAccessToken(
+//           code,
+//           client_id,
+//           client_secret
+//         )
+//       const response: OAuthAuthorizationToAccessTokenRequestResponse = {
+//         accessToken: fromServerResponse.access_token,
+//       }
+//       res.status(200).json(response)
+//     } catch (err) {
+//       if (err instanceof AxiosError) {
+//         const status = err.response?.status
+//         const data = err.response?.data
+//         res.status(status ?? 500).json(data)
+//       }
+//     }
+//   }
+// )
 
 export default oauthHandler
