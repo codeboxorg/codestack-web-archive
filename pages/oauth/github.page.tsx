1f81c7fca02d332d97d193b004ced269d049d969
김정윤 <kjyjh9930@gmail.com>
2022-12-10 20:31:09 +0900

    style : client에서 사용하는 api 서비스 폴더네임 변경

--- pages/oauth/github.page.tsx
+++ pages/oauth/github.page.tsx
@@ -1,4 +1,4 @@
-import { api } from 'apiForClient/index'
+import { api } from 'api/index'
 import LoadingDot from '@components/shared/LoadingDot'
 import { useRouter } from 'next/router'
 import { useEffect } from 'react'
