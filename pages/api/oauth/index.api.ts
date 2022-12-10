5bc38361f72b7d0ad165b4a3dfd896f3736fabf0
김정윤 <kjyjh9930@gmail.com>
2022-12-10 16:03:08 +0900

    test : vercel 프로덕션 배포 환경에서의 api 테스트용도 코드 추가

--- pages/api/oauth/index.api.ts
+++ pages/api/oauth/index.api.ts
@@ -3,3 +3,5 @@ import { NextApiRequest, NextApiResponse } from 'next'
 function handler(req: NextApiRequest, res: NextApiResponse) {
   res.status(200).json({ test: '/oauth/' })
 }
+
+export default handler
