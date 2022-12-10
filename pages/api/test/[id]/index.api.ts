6c5b11d2bb08e2b8643b319fb060b06e96fff5c4
김정윤 <kjyjh9930@gmail.com>
2022-12-10 21:03:05 +0900

    feat : next api 동적 path 재구성 #4

--- pages/api/test/[id]/index.api.ts
+++
@@ -1,7 +0,0 @@
-import { NextApiRequest, NextApiResponse } from 'next'
-
-function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json({ test: '/dynamic test/' })
-}
-
-export default handler
