628e52bbe0efa277539929987b51188b892724d5
김정윤 <kjyjh9930@gmail.com>
2022-12-10 20:56:51 +0900

    feat : next api 동적 path 재구성 #4

--- pages/api/oauth/index.api.ts
+++
@@ -1,7 +0,0 @@
-import { NextApiRequest, NextApiResponse } from 'next'
-
-function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json({ test: '/oauth/' })
-}
-
-export default handler
