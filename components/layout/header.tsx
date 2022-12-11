d6ba8d55f3a4ed5af21cb7d9267f4c9d2270492e
rrrmaster <dudgus9101@gmail.com>
2022-12-12 02:58:58 +0900

    feat: '레이아웃 작업'

---
+++ components/layout/header.tsx
@@ -0,0 +1,26 @@
+import Link from 'next/link'
+
+const Header = () => {
+  return (
+    <header>
+      <nav className="flex items-center justify-between flex-wrap bg-blue-500 p-6">
+        <div className="flex items-center flex-shrink-0 text-white mr-6">
+          <Link href="/" className="font-semibold text-xl tracking-tight">
+            Codebox
+          </Link>
+        </div>
+        <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+          <div className="text-sm lg:flex-grow">
+            <Link
+              href="/problem"
+              className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-blue-200 mr-4"
+            >
+              문제
+            </Link>
+          </div>
+        </div>
+      </nav>
+    </header>
+  )
+}
+export default Header
