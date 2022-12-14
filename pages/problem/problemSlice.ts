2a07f8cce70be25b47a03e04854e0f5bfd3dcb08
김정윤 <kjyjh9930@gmail.com>
2022-12-14 23:22:29 +0900

    feat : problem detail 페이지에서 props drilling를 해소하기위해 SSR시 리덕스를 이용하도록 구현

---
+++ pages/problem/problemSlice.ts
@@ -0,0 +1,32 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import { RootState } from '@store/configureStore'
+
+const problemSliceType = 'slice/problem'
+
+export interface ProblemSlice {
+  problem: Problem
+}
+
+const initialState: ProblemSlice = {
+  problem: {
+    id: -1,
+    title: '',
+    context: '',
+    max_cpu_time: 0,
+    max_memory: 0,
+  },
+}
+
+const problemSlice = createSlice({
+  initialState,
+  name: problemSliceType,
+  reducers: {
+    setProblem: (state, action: PayloadAction<Problem>) => {
+      state.problem = action.payload
+    },
+  },
+})
+
+export const { setProblem } = problemSlice.actions
+export const getProblem = (state: RootState) => state.problem.problem
+export default problemSlice
