50ef385e2d8c3411849cf84013b3414bc67f8652
rrrmaster <dudgus9101@gmail.com>
2022-12-17 16:27:43 +0900

    fix:로그인 페이지 변경

--- pages/index.page.tsx
+++ pages/index.page.tsx
@@ -3,7 +3,6 @@ import Link from 'next/link'
 const Landing = () => {
   return (
     <>
-      <Link href={'/loginPage'}>로그인 페이지로</Link>
       <p>
         <Link href={'/problem'}>문제 목록 페이지로</Link>
       </p>
