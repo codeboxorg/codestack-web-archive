47018e5adc7bb8193966918c22ae9e89bcaebd27
jeongyun kim <jeongyunniim@gmail.com>
2023-02-16 15:05:34 +0900

    feat : 에러페이지 작성

---
+++ pages/error.page.tsx
@@ -0,0 +1,14 @@
+import Link from 'next/link'
+
+const Error = () => {
+  return (
+    <>
+      <p>에러페이지</p>
+      <p>
+        <Link href={'/'}>홈으로 돌아가기</Link>
+      </p>
+    </>
+  )
+}
+
+export default Error
