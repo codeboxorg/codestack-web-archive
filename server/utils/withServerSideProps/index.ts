0299a72e89a3d0c42c117a1f9098d43667847fd8
김정윤 <kjyjh9930@gmail.com>
2023-02-22 15:28:50 +0900

    feat : withGetServerSideProps에서 리프레시토큰을 검증 후 유저를 받아오는 로직 작성

--- server/utils/withServerSideProps/index.ts
+++ server/utils/withServerSideProps/index.ts
@@ -1,11 +1,31 @@
+import { baseAPI } from '@api/core'
 import { RemoteError } from '@api/error/remoteError'
-import { GetServerSideProps, GetServerSidePropsContext } from 'next'
+import { setLoginUser } from '@components/auth/authSlice'
+import wrapper from '@store/configureStore'
+import { getCookie } from 'cookies-next'
+import { GetServerSideProps } from 'next'
 
 export default function withGetServerSideProps(
   getServerSideProps: GetServerSideProps
 ): GetServerSideProps {
-  return async (context: GetServerSidePropsContext) => {
+  return wrapper.getServerSideProps((store) => async (context) => {
     try {
+      const { req, res } = context
+      const refreshToken = getCookie('server-key', { req, res })
+      /**
+       * TODO : 이곳에서 refresh token을 검증후 유저를 받아옴 현재는 임시로 토큰 검증 완료를 가정
+       */
+      if (refreshToken === 'temp.token') {
+        store.dispatch(
+          setLoginUser({
+            userName: 'temp',
+            userProfileImageUrl: 'temp.user.imageUrl',
+            userEmail: 'temp.user.email',
+            accessToken: 'temp.user.token',
+          })
+        )
+        baseAPI.setDefaultAuthorizationHeader('temp.user.token')
+      }
       return await getServerSideProps(context)
     } catch (error) {
       if (!(error instanceof RemoteError)) {
@@ -18,5 +38,5 @@ export default function withGetServerSideProps(
         },
       }
     }
-  }
+  })
 }
