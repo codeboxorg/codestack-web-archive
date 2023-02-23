0a3a01ba303a8ceae444c3da85c769acd9e0ee3c
김정윤 <kjyjh9930@gmail.com>
2023-02-23 14:01:21 +0900

    feat : memberService 추가

--- apiService/index.ts
+++ apiService/index.ts
@@ -1,14 +1,17 @@
 import { authServiceRemote } from './service/authService/index'
+import { memberServiceRemote } from './service/memberService'
 import { problemServiceRemote } from './service/problemService'
 import { submissionServiceRemote } from './service/submissionService'
 const provideAPIService = () => {
   const authService = authServiceRemote()
   const problemService = problemServiceRemote()
   const submissionService = submissionServiceRemote()
+  const memberService = memberServiceRemote()
   return {
     authService,
     problemService,
     submissionService,
+    memberService,
   }
 }
 
