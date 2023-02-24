e2e92a860c247d96b5115d9e11ebd16511d4b089
김정윤 <kjyjh9930@gmail.com>
2023-02-24 22:13:42 +0900

    feat : submission 도메인 쿼리키 constant 추가

---
+++ pages/submission/queryKey.ts
@@ -0,0 +1,9 @@
+const submissionKeys = {
+  all: ['submission'] as const,
+  lists: () => [...submissionKeys.all, 'list'] as const,
+  list: (pageNum: number, filters: string) =>
+    [...submissionKeys.lists(), { filters, pageNum }] as const,
+  detail: () => [...submissionKeys.all, 'detail'] as const,
+}
+
+export { submissionKeys }
