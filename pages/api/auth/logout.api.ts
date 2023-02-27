16313cbedef583f0da07269a020810a3ad40ef32
김정윤 <kjyjh9930@gmail.com>
2023-02-27 23:37:32 +0900

    style : TODO 주석 작성

--- pages/api/auth/logout.api.ts
+++ pages/api/auth/logout.api.ts
@@ -5,6 +5,7 @@ import { deleteCookie } from 'cookies-next'
 
 authHandler.post<NextRequest<LoginForm>>(async (req, res) => {
   deleteCookie('server-key', { req, res })
+  //TODO : 실제 Remote 서버에도 key 삭제 요청 보내기
   res.status(200).json(true)
 })
 
