15e3810e841993de7ff59803b8ba366ec12e74db
김정윤 <kjyjh9930@gmail.com>
2023-02-28 00:40:36 +0900

    feat : 메시지 없이 로그아웃 로그인 메서드를 호출 할 수 있게 옵션으로 추가

--- hooks/useAuth/index.tsx
+++ hooks/useAuth/index.tsx
@@ -2,10 +2,26 @@ import { api } from '@api/index'
 import { getLoginUser, setLoginUser } from '@components/auth/authSlice'
 import { useMutation } from '@tanstack/react-query'
 import { MESSAGE } from 'constant/message'
-import { message } from 'antd'
+import { message as messageCall } from 'antd'
 import { useRouter } from 'next/router'
 import { useCallback } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
+
+type LoginMethodOption = {
+  message: boolean
+}
+
+type LogoutMethodOption = {
+  message?: boolean
+}
+
+const loginDefaultOption: LoginMethodOption = {
+  message: false,
+}
+
+const logoutDefaultOption: LogoutMethodOption = {
+  message: false,
+}
 
 const useAuth = () => {
   const user = useSelector(getLoginUser)
@@ -13,10 +29,13 @@ const useAuth = () => {
 
   const dispatch = useDispatch()
 
-  const login = useCallback((user: LoginMember) => {
-    dispatch(setLoginUser(user))
-    message.success(MESSAGE.authMessage.success.login)
-  }, [])
+  const login = useCallback(
+    (user: LoginMember, option: LoginMethodOption = loginDefaultOption) => {
+      dispatch(setLoginUser(user))
+      option.message && messageCall.success(MESSAGE.authMessage.success.login)
+    },
+    []
+  )
 
   const logoutMutation = useMutation(api.authService.logout, {
     onSuccess: () => {
@@ -25,10 +44,13 @@ const useAuth = () => {
     },
   })
 
-  const logout = useCallback(() => {
-    logoutMutation.mutate()
-    message.success(MESSAGE.authMessage.success.logout)
-  }, [])
+  const logout = useCallback(
+    (option: LogoutMethodOption = logoutDefaultOption) => {
+      logoutMutation.mutate()
+      option.message && messageCall.success(MESSAGE.authMessage.success.logout)
+    },
+    []
+  )
 
   const getMemberMutation = useMutation(api.authService.member, {
     onSuccess: (user) => login(user),
