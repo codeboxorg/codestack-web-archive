7948905062368336b8eba94df13bd844e07b2aa9
김정윤 <kjyjh9930@gmail.com>
2023-02-27 22:39:07 +0900

    feat : useAuth 메서드에 useCallback 적용 및 로그아웃 기능 실제 연결

--- hooks/useAuth/index.tsx
+++ hooks/useAuth/index.tsx
@@ -1,32 +1,37 @@
 import { api } from '@api/index'
 import { getLoginUser, setLoginUser } from '@components/auth/authSlice'
 import { useMutation } from '@tanstack/react-query'
+import { useRouter } from 'next/router'
+import { useCallback } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 
 const useAuth = () => {
   const user = useSelector(getLoginUser)
+  const router = useRouter()
+
   const dispatch = useDispatch()
 
-  const mutation = useMutation(api.authService.member)
-
-  const login = (user: LoginMember) => {
+  const login = useCallback((user: LoginMember) => {
     dispatch(setLoginUser(user))
-  }
+  }, [])
 
-  const logout = () => {
-    dispatch(setLoginUser(false))
-  }
+  const logoutMutation = useMutation(api.authService.logout, {
+    onSuccess: () => {
+      dispatch(setLoginUser(false))
+      router.push('/')
+    },
+  })
 
-  const initMember = () => {
-    mutation.mutate(undefined, {
-      onSuccess(user) {
-        login(user)
-      },
-      onError() {
-        logout()
-      },
-    })
-  }
+  const logout = useCallback(() => {
+    logoutMutation.mutate()
+  }, [])
+
+  const getMemberMutation = useMutation(api.authService.member, {
+    onSuccess: (user) => login(user),
+    onError: logout,
+  })
+
+  const initMember = useCallback(() => getMemberMutation.mutate(), [])
 
   return {
     user,
