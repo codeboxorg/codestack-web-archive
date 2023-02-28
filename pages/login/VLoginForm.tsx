75185a59c2cdf86697292da8fa1fb4a90ae53d2c
김정윤 <kjyjh9930@gmail.com>
2023-02-28 21:05:17 +0900

    feat : Submit 버튼이 Form 하위에 포함되게 이동

--- pages/login/VLoginForm.tsx
+++ pages/login/VLoginForm.tsx
@@ -66,6 +66,12 @@ const VLoginForm = ({
             <p className="text-red-500 text-xs">{passwordInput.message}</p>
           )}
         </div>
+        <button
+          type="submit"
+          className="w-full mt-20 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm m-auto block px-5 py-2.5 text-center"
+        >
+          로그인
+        </button>
       </div>
     </form>
   )
--- pages/login/index.page.tsx
+++ pages/login/index.page.tsx
@@ -7,13 +7,6 @@ const LoginPage = () => {
       <div className="sm:w-full md:w-450 mx-auto">
         <h1 className="text-center text-25 my-30">로그인</h1>
         <LoginForm />
-        <button
-          form="login"
-          type="submit"
-          className="w-full mt-20 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm m-auto block px-5 py-2.5 text-center"
-        >
-          로그인
-        </button>
         <div className="mt-20">
           <Link
             href="http://dev-api.rrrmaster.com/oauth2/authorization/github?redirect_uri=https://pr-16.dp9i34mgnqryi.amplifyapp.com/api/oauth2/redirect"
