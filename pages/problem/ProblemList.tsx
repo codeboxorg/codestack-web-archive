79458a0bb409122125bd1e5cc695b84508a8ef96
김정윤 <kjyjh9930@gmail.com>
2023-03-03 23:17:32 +0900

    feat : ProblemList VAC 디버거 삭제후 실제 뷰 컴포넌트 props 전달 작업

--- pages/problem/ProblemList.tsx
+++ pages/problem/ProblemList.tsx
@@ -1,32 +1,45 @@
 import VProblemList from './VProblemList'
-import dynamic from 'next/dynamic'
-import { Suspense } from 'react'
 import { useRouter } from 'next/router'
-
-const VAC = dynamic(import('react-vac'), { ssr: false })
 
 type Props = {
   list: Problem[]
 }
 
+const columns = [
+  {
+    title: '문제 번호',
+    dataIndex: 'id',
+    key: 'id',
+  },
+  {
+    title: '문제 제목',
+    dataIndex: 'title',
+    key: 'title',
+  },
+  {
+    title: '제출',
+    dataIndex: 'submission',
+    key: 'submission',
+  },
+  {
+    title: '정답',
+    dataIndex: 'accepted',
+    key: 'accepted',
+  },
+]
+
 const ProblemList = ({ list }: Props) => {
   const router = useRouter()
   const vProblemListProps = {
-    list: list.map((data, idx) => ({
+    list: list.map((data) => ({
       ...data,
       handleRowClick: () => {
         router.push(`/problem/${data.id}`)
       },
     })),
+    columns,
   }
-  return (
-    <>
-      <VProblemList />
-      <Suspense>
-        <VAC name="VProblemList" data={vProblemListProps} useList="list" />
-      </Suspense>
-    </>
-  )
+  return <VProblemList {...vProblemListProps} />
 }
 
 export default ProblemList
