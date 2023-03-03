3e5c9e1abb5c52c46110f586063dafba9092cd7a
김정윤 <kjyjh9930@gmail.com>
2023-03-03 18:58:52 +0900

    feat : Menu VAC 컴포넌트 작성

---
+++ components/layout/Header/VMenu.tsx
@@ -0,0 +1,33 @@
+import classNames from 'classnames'
+import { ROUTE_PATH } from 'constant/routePath'
+import Link from 'next/link'
+
+export type MenuItem = {
+  href: ROUTE_PATH | string
+  name: string
+  isHighlight: boolean
+}
+
+export type VMenuProps = {
+  menus: MenuItem[]
+}
+
+const VMenu = ({ menus }: VMenuProps) => {
+  return (
+    <>
+      {menus.map(({ href, name, isHighlight }) => (
+        <Link
+          className={classNames(
+            'block hover:text-blue-500 lg:inline-block',
+            isHighlight ? 'text-blue-500' : 'text-black'
+          )}
+          href={href}
+        >
+          {name}
+        </Link>
+      ))}
+    </>
+  )
+}
+
+export default VMenu
