fe35a6109ce960ef3866a8e5c5097f32564b9692
김정윤 <kjyjh9930@gmail.com>
2023-03-03 22:29:26 +0900

    design : 헤더 레이아웃 변경 (container class 적용)

--- components/layout/Header/index.tsx
+++ components/layout/Header/index.tsx
@@ -14,7 +14,7 @@ const Header = () => {
           '0px 5.89px 5.13px rgba(0, 0, 0, 0.0212), 0px 3.7px 3.23px rgba(0, 0, 0, 0.0182), 0px 2.13px 1.86px rgba(0, 0, 0, 0.0149), 0px 0.94px 0.82px rgba(0, 0, 0, 0.0104)',
       }}
     >
-      <nav className="h-full flex flex-wrap items-center py-6 gap-x-10 md:flex-nowrap lg:flex-nowrap pt-7 px-30">
+      <nav className="h-full container flex flex-wrap items-center py-6 gap-x-30 md:flex-nowrap lg:flex-nowrap pt-7 px-30">
         <div className="flex items-center flex-shrink-0 text-white mr-10">
           <Link
             href="/"
