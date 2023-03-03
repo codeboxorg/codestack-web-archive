b825c6dd80f6d166e88f9e1bab144942dfdd932e
김정윤 <kjyjh9930@gmail.com>
2023-03-03 23:15:56 +0900

    design : 아바타 크기 변경

--- components/layout/Header/MemberMenu.tsx
+++ components/layout/Header/MemberMenu.tsx
@@ -33,7 +33,7 @@ const MemberMenu = ({ profileImage }: Props) => {
     <>
       <VMemberMenu {...vMemberMenuProps}>
         <img
-          className="rounded-full w-35 h-35 cursor-pointer"
+          className="rounded-full w-40 h-40 cursor-pointer"
           src={profileImage}
           alt="Picture of the author"
         />
