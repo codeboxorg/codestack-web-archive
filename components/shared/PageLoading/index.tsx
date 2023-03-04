1162583f8b6bd5b68d2955be59456f29933112f7
김정윤 <kjyjh9930@gmail.com>
2023-03-04 17:03:19 +0900

    feat : 기본 로딩 폴백으로 사용할 컴포넌트 작성

---
+++ components/shared/PageLoading/index.tsx
@@ -0,0 +1,14 @@
+import LoadingDot from '../LoadingDot'
+
+const PageLoading = () => {
+  return (
+    <div
+      style={{ zIndex: '9999' }}
+      className="fixed bg-black/25 top-0 left-0 right-0 bottom-0 flex flex-center"
+    >
+      <LoadingDot />
+    </div>
+  )
+}
+
+export default PageLoading
