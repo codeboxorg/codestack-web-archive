410cb9ee9b740f028243ea78a5ad30e12e40cad5
김정윤 <kjyjh9930@gmail.com>
2023-03-04 11:37:48 +0900

    design : PaginationBar 퍼블리싱 작업

--- components/shared/PaginationBar/VPaginationBar.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const VPaginationBar = () => {
-  return <></>
-}
-
-export default VPaginationBar
--- components/shared/PaginationBar/index.tsx
+++ components/shared/PaginationBar/index.tsx
@@ -1,32 +1,58 @@
-import dynamic from 'next/dynamic'
-import { Suspense } from 'react'
+import classNames from 'classnames'
 import { usePaginationReturn } from 'react-use-pagination-hook'
-import VPaginationBar from './VPaginationBar'
 
-const VAC = dynamic(import('react-vac'), { ssr: false })
-
-const PaginationBar = ({ pagelist, setPage, ...rest }: usePaginationReturn) => {
-  const vPaginationBarProps = {
-    ...rest,
-    pagelist: pagelist.map((page) => ({
-      page,
-      handlePageClick: () => {
-        setPage(page)
-      },
-    })),
-  }
-
+const PaginationBar = ({
+  pageList,
+  goNextSection,
+  goBeforeSection,
+  goNext,
+  goBefore,
+  setPage,
+  currentPage,
+}: usePaginationReturn) => {
   return (
-    <>
-      <VPaginationBar />
-      <Suspense>
-        <VAC
-          name="VPaginationBar"
-          data={vPaginationBarProps}
-          useList="pagelist"
-        />
-      </Suspense>
-    </>
+    <div className="flex gap-5 all:rounded-md p-10">
+      <button
+        className="h-35 w-35 hover:bg-neutral-100 transition-all duration-300"
+        onClick={() => goBeforeSection()}
+      >
+        {'<<'}
+      </button>
+      <button
+        className="h-35 w-35 hover:bg-neutral-100 transition-all duration-300"
+        onClick={() => goBefore()}
+      >
+        {'<'}
+      </button>
+      <ul className="flex gap-5 all:cursor-pointer all:h-35 all:w-35">
+        {pageList.map((page) => (
+          <li
+            onClick={() => setPage(page)}
+            className={classNames(
+              'flex justify-center items-center transition-all duration-300',
+              page === currentPage
+                ? 'border-1 border-blue-500 text-blue-500'
+                : 'hover:bg-neutral-100'
+            )}
+            key={page}
+          >
+            {page}
+          </li>
+        ))}
+      </ul>
+      <button
+        className="h-35 w-35 hover:bg-neutral-100 transition-all duration-300"
+        onClick={() => goNext()}
+      >
+        {'>'}
+      </button>
+      <button
+        className="h-35 w-35 hover:bg-neutral-100 transition-all duration-300"
+        onClick={() => goNextSection()}
+      >
+        {'>>'}
+      </button>
+    </div>
   )
 }
 
