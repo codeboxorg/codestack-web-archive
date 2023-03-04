313ca546a63e9c53c4b8793e50902a4ac348b831
김정윤 <kjyjh9930@gmail.com>
2023-03-04 17:08:25 +0900

    design : 레이아웃 조정

--- pages/register/index.page.tsx
+++ pages/register/index.page.tsx
@@ -3,7 +3,7 @@ import RegisterForm from './RegisterForm'
 const RegisterPage = () => {
   return (
     <>
-      <main className="container px-20 w-full pt-100">
+      <main className="container px-20 w-full pt-[7vh]">
         <div className="sm:w-full md:w-450 mx-auto">
           <h1 className="text-center text-25 my-30">회원가입</h1>
           <RegisterForm />
