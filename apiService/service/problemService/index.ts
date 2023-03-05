8712dc2eac880befc399b27cf375ed459db0d652
김정윤 <kjyjh9930@gmail.com>
2023-03-05 16:18:51 +0900

    feat : 문제 제출 서비스 메서드 구현

--- apiService/service/problemService/index.ts
+++ apiService/service/problemService/index.ts
@@ -1,6 +1,7 @@
 import { baseAPI } from 'apiService/core'
 import { throwRemoteError } from 'apiService/error/remoteError'
 import { ProblemService } from './problemService'
+import { Submit } from '@pages/problem/[id]/submit/index.page'
 
 export const problemServiceRemote = (): ProblemService => ({
   problemList: async (page) => {
@@ -14,10 +15,21 @@ export const problemServiceRemote = (): ProblemService => ({
       throwRemoteError(error)
     }
   },
-  problemDetail: async (id: number) => {
+  problemDetail: async (id) => {
     try {
       const response = await baseAPI.get({
         url: `/problem/${id}`,
+      })
+      return response.data
+    } catch (error) {
+      throwRemoteError(error)
+    }
+  },
+  problemSubmit: async (submit) => {
+    try {
+      const response = await baseAPI.post({
+        url: `/submission`,
+        data: submit,
       })
       return response.data
     } catch (error) {
--- apiService/service/problemService/problemService.ts
+++ apiService/service/problemService/problemService.ts
@@ -1,4 +1,7 @@
+import { Submit } from '@pages/problem/[id]/submit/index.page'
+
 export interface ProblemService {
   problemList(page: number): Promise<Pagination<Problem>>
   problemDetail(id: number): Promise<Problem>
+  problemSubmit(submit: Submit): Promise<unknown>
 }
