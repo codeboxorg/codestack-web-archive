c97e3b909e911beec4c05c3655ca4b4ca81d3e47
김정윤 <kjyjh9930@gmail.com>
2023-03-05 16:35:37 +0900

    feat : 제출 결과 코드 타입 추가

--- types/submission.d.ts
+++ types/submission.d.ts
@@ -1,3 +1,5 @@
+type SubmissionStatus = 'AC' | 'WA' | 'PE' | 'TLE' | 'MLE' | 'OLE' | 'RE'
+
 interface Submission {
   id: number
   source_code: string
@@ -6,4 +8,5 @@ interface Submission {
   problem: Pick<Problem, 'id' | 'title'>
   member: Pick<User, 'id' | 'nickname'>
   language: Pick<Language, 'id' | 'name'>
+  status_code: SubmissionStatus
 }
