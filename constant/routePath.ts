6929a37b43f4c6df879ebbb61de7cf0b3157867d
김정윤 <kjyjh9930@gmail.com>
2023-03-15 23:16:45 +0900

    feat : ALLOWED_ONLY_TO_MEMBERS routePath constant로 이동 처리

--- constant/routePath.ts
+++ constant/routePath.ts
@@ -5,5 +5,9 @@ export const ROUTE_PATH = {
   PROBLEM: '/problem',
   SUBMISSION: '/submission',
 } as const
-
 export type ROUTE_PATH = Values<typeof ROUTE_PATH>
+
+export const ALLOWED_ONLY_TO_MEMBERS = [
+  '/submission',
+  '/problem/[id]/submit',
+] as const
