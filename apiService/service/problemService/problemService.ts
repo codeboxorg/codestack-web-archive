de731ea4fdc412159d97e11a5436f8087f3d88c1
김정윤 <kjyjh9930@gmail.com>
2023-03-15 22:52:21 +0900

    feat : 문제 제출 반환타입 적용

--- apiService/service/problemService/problemService.ts
+++ apiService/service/problemService/problemService.ts
@@ -3,5 +3,5 @@ import { Submit } from '@pages/problem/[id]/submit/index.page'
 export interface ProblemService {
   problemList(page: number): Promise<Pagination<Problem>>
   problemDetail(id: number): Promise<Problem>
-  problemSubmit(submit: Submit): Promise<unknown>
+  problemSubmit(submit: Submit): Promise<SubmitRes>
 }
