0b4dcaeb808e675089f5c129a9e8f288f9628122
김정윤 <kjyjh9930@gmail.com>
2023-03-15 22:49:04 +0900

    feat : 문제 제출 반환 타입 추가

--- types/problem.d.ts
+++ types/problem.d.ts
@@ -8,3 +8,13 @@ interface Problem {
   accepted: number
   language: Language[]
 }
+
+type SubmitRes = {
+  id: number
+  language_id: number
+  member_id: number
+  problem_id: number
+  source_code: string
+  updated_at: string
+  created_at: string
+}
