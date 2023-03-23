b5af28a93293f11fecb63d340d8031bd9ecc2747
김정윤 <kjyjh9930@gmail.com>
2023-03-23 22:07:09 +0900

    feat : GA 등록 컴포넌트 작성 및 등록

---
+++ components/app/GA/index.tsx
@@ -0,0 +1,21 @@
+const GA = () => {
+  return (
+    <>
+      <script
+        async
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+      ></script>
+      <script
+        id="google-analytics"
+        dangerouslySetInnerHTML={{
+          __html: `window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');`,
+        }}
+      ></script>
+    </>
+  )
+}
+
+export default GA
---
+++ pages/_document.page.tsx
@@ -0,0 +1,18 @@
+import GA from '@components/app/GA'
+import { Html, Head, Main, NextScript } from 'next/document'
+
+const Document = () => {
+  return (
+    <Html>
+      <Head>
+        <GA />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
+
+export default Document
