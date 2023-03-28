851117e7729bbfe76095a0bea9a803177ed2248c
김정윤 <kjyjh9930@gmail.com>
2023-03-28 22:50:29 +0900

    design : 배너 텍스트 반응형 적용

--- pages/MainBanner.tsx
+++ pages/MainBanner.tsx
@@ -17,7 +17,7 @@ const MainBanner = () => {
       </div>
       <div
         className={
-          'absolute w-full top-65 left-0 flex flex-center flex-col z-40 text-white h-[60vh]'
+          'absolute w-full top-65 left-0 flex flex-center flex-col z-40 text-white h-[60vh] px-10'
         }
       >
         <h1 className="text-30 sm:text-40 md:text-50 font-bold">
@@ -26,7 +26,7 @@ const MainBanner = () => {
         <h1 className="text-30 sm:text-40 md:text-50 font-bold">
           코딩테스트 연습 플랫폼
         </h1>
-        <h3 className="font-semibold mt-20">
+        <h3 className="text-13 sm:text-base font-semibold mt-20">
           CodeStack과 함께 목표를 설정하고 같이 문제를 풀어나가보아요!
         </h3>
       </div>
