bba833100e302cab331ac4d3ccdb5e5cfbec5f66
김정윤 <kjyjh9930@gmail.com>
2023-04-03 13:43:40 +0900

    feat : svgr webpack rule 적용

--- next.config.js
+++ next.config.js
@@ -11,6 +11,13 @@ const nextConfig = {
       },
     ]
   },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    })
+    return config
+  },
   env: {
     NEXT_SERVER_BASE_API_URL: process.env.NEXT_SERVER_BASE_API_URL,
     NEXT_PUBLIC_GITHUB_CLIENT_ID: process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID,
