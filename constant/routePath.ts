8d26feb21f42074afbee307744edb128c4059ade
je0ngyun <kjyjh9930@gmail.com>
2023-05-15 22:23:41 +0900

    feat: 활용성이 낮은 RoutePath 오버라이딩 관련 코드삭제

--- constant/routePath.ts
+++ constant/routePath.ts
@@ -1,12 +1,3 @@
-export const ROUTE_PATH = {
-  LANDING: '/',
-  HOME: '/home',
-  LOGIN: '/login',
-  PROBLEM: '/problem',
-  SUBMISSION: '/submission',
-} as const
-export type ROUTE_PATH = Values<typeof ROUTE_PATH>
-
 export const ALLOWED_ONLY_TO_MEMBERS = [
   '/submission',
   '/problem/[id]/submit',
--- types/module.d.ts
+++
@@ -1,9 +0,0 @@
-import { NextRouter } from 'next/router'
-
-type RoutePaths = import('constant/routePath').ROUTE_PATH
-
-type RoutePushMethod = (path: RoutePaths) => Promise<boolean>
-
-declare module 'next/router' {
-  function useRouter(): NextRouter & { push: RoutePushMethod }
-}
