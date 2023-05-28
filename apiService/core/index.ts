034b83dab25a12282335d2118e15234ccabe5e73
je0ngyun <kjyjh9930@gmail.com>
2023-05-28 19:15:40 +0900

    style : 네이밍 변경 APICreater -> ApiCreater

--- apiService/core/index.ts
+++ apiService/core/index.ts
@@ -1,15 +1,15 @@
 import renderEnv from '@utils/renderEnv'
-import { APICreator } from './APICreator'
+import { ApiCreator } from './ApiCreator'
 
 //next 자체서버 (pages/api)로의 요청용
-const nextAPI = new APICreator(`/`)
+const nextAPI = new ApiCreator(`/`)
 
 /**
  * 메인 api 서버 요청용 dev모드에서 SSR시와 CSR시 proxy 사용 유무가 달라지므로 분기
  * CSR시에 BASE_API_URL은 undefined이므로 /proxy 사용 URL로 변경된다.
  */
 const baseAPI = renderEnv.isSSR
-  ? new APICreator(`${process.env.NEXT_SERVER_BASE_API_URL}`)
-  : new APICreator(`${process.env.NEXT_PUBLIC_BASE_API_URL}`)
+  ? new ApiCreator(`${process.env.NEXT_SERVER_BASE_API_URL}`)
+  : new ApiCreator(`${process.env.NEXT_PUBLIC_BASE_API_URL}`)
 
 export { baseAPI, nextAPI }
