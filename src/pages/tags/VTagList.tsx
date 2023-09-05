94dd1a8ef481d9a2b6fd87ab13100221bd2b3d07
je0ngyun <kjyjh9930@gmail.com>
2023-09-05 21:52:34 +0900

    feat: tag list 페이지 VAC 패턴 적용

---
+++ src/pages/tags/VTagList.tsx
@@ -0,0 +1,38 @@
+import BaseTable from '@components/shared/BaseTable'
+import { ColumnsType } from 'antd/es/table'
+
+type TagRow = Tag
+type TagColumns = ColumnsType<TagRow>
+
+export type VTagListProps = {
+    list: TagRow[]
+}
+
+const columns: TagColumns = [
+    {
+        title: '태그',
+        dataIndex: 'name',
+        key: 'name',
+    },
+    // TODO: 서버측 문제 수 field 개발 완료 시 교체
+    {
+        title: '문제 수',
+        dataIndex: 'id',
+        key: 'id',
+    },
+]
+
+function VSubmissionList({ list }: VTagListProps) {
+    return (
+        <BaseTable
+            className='border-1 border-neutral-200 rounded-md px-1'
+            rowClassName='cursor-pointer'
+            rowKey={(row) => row.id}
+            dataSource={list}
+            columns={columns}
+            pagination={false}
+        />
+    )
+}
+
+export default VSubmissionList
--- src/pages/tags/index.page.tsx
+++ src/pages/tags/index.page.tsx
@@ -1,24 +1,8 @@
 import PaginationBar from '@components/shared/PaginationBar'
 import { useTagList } from '@hooks/tag/useTagList'
-import { Table } from 'antd'
-import type { ColumnsType } from 'antd/es/table'
-import Link from 'next/link'
+import { NextSeo } from 'next-seo'
 import usePagination from 'react-use-pagination-hook'
-
-const columns: ColumnsType<Tag> = [
-    {
-        title: '태그',
-        dataIndex: 'name',
-        key: 'name',
-        render: (text, record) => <Link href={`/tags/${record.id}`}>{text}</Link>,
-    },
-    {
-        // TODO : 나중에 수정하면 변경 필요
-        title: '문제 수',
-        dataIndex: 'id',
-        key: 'id',
-    },
-]
+import TagList from './TagList'
 
 function TagsPage() {
     const paginationMethods = usePagination({ numOfPage: 5 })
@@ -33,12 +17,15 @@ function TagsPage() {
     })
 
     return (
-        <div className='pt-50'>
-            <Table columns={columns} dataSource={tagListPagination?.content} pagination={false} />
-            <div className='w-full flex justify-center py-30'>
-                <PaginationBar {...paginationMethods} />
+        <>
+            <NextSeo title='제출 근황' />
+            <div className='pt-50'>
+                <TagList list={tagListPagination?.content ?? []} />
+                <div className='w-full flex justify-center py-30'>
+                    <PaginationBar {...paginationMethods} />
+                </div>
             </div>
-        </div>
+        </>
     )
 }
 
