029abe4714e2730a41743ccc228c0ed5aa14747f
je0ngyun <kjyjh9930@gmail.com>
2023-10-05 22:06:52 +0900

    feat: usePaginationParams 작성

--- src/hooks/shared/index.ts
+++ src/hooks/shared/index.ts
@@ -1,3 +1,4 @@
 export * from './useAuth'
 export * from './useRootState'
 export * from './useClipboard'
+export * from './usePaginationParams'
---
+++ src/hooks/shared/usePaginationParams.ts
@@ -0,0 +1,23 @@
+import { usePathname, useSearchParams } from 'next/navigation'
+import { useRouter } from 'next/router'
+import { useCallback } from 'react'
+
+export const usePaginationParams = (): [number, typeof setPage] => {
+    const pathname = usePathname()
+    const searchParams = useSearchParams()
+    const router = useRouter()
+
+    const currentPage = Number(searchParams.get('page')) || 0
+
+    const setPage = useCallback(
+        (value: number) => {
+            const newParams = new URLSearchParams(searchParams.toString())
+            newParams.set('page', value.toString())
+
+            router.push({ pathname, query: newParams.toString() })
+        },
+        [pathname, router, searchParams],
+    )
+
+    return [currentPage, setPage]
+}
