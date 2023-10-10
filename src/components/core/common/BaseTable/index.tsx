5460a1ac527b82fc05be9d797a42396e4b777c37
je0ngyun <kjyjh9930@gmail.com>
2023-10-10 21:32:06 +0900

    feat: defaultStyle twin 사용으로 변경

--- src/components/core/common/BaseTable/index.tsx
+++ src/components/core/common/BaseTable/index.tsx
@@ -1,10 +1,9 @@
 import { Table, TableProps } from 'antd'
-import { CSSProperties } from 'react'
+import tw from 'twin.macro'
+
+const defaultStyle = tw`min-w-500`
 
 function BaseTable<RowType extends Record<string, any>>({ style, ...props }: TableProps<RowType>) {
-    const defaultStyle: CSSProperties = {
-        minWidth: '500px',
-    }
     return (
         <div className='overflow-x-scroll border-1 border-neutral-200 rounded-md'>
             <Table<RowType> style={{ ...defaultStyle, ...style }} {...props} />
