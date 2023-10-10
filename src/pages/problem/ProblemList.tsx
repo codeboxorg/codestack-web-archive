f5fe14cc5977c2991e61784972a4a97ae2369dd7
je0ngyun <kjyjh9930@gmail.com>
2023-10-10 21:54:13 +0900

    refactor: 문제 목록 VAC 패턴 제거 및 리펙토링

--- src/pages/problem/ProblemList.tsx
+++ src/pages/problem/ProblemList.tsx
@@ -1,23 +1,68 @@
+import { BaseTable } from '@components/core/common'
 import { getPercentage } from '@utils/percentage'
+import { ColumnsType } from 'antd/es/table'
 import { useRouter } from 'next/router'
-import VProblemList, { VProblemProps } from './VProblemList'
 
-type Props = {
+interface Props {
     list?: Problem[]
 }
 
+interface ProblemRow extends Problem {
+    acceptPer: string
+}
+
+type ProblemColumns = ColumnsType<ProblemRow>
+
+const tableColumns: ProblemColumns = [
+    {
+        title: '문제 번호',
+        dataIndex: 'id',
+        key: 'id',
+    },
+    {
+        title: '문제 제목',
+        dataIndex: 'title',
+        key: 'title',
+    },
+    {
+        title: '제출',
+        dataIndex: 'submission',
+        key: 'submission',
+    },
+    {
+        title: '정답',
+        dataIndex: 'accepted',
+        key: 'accepted',
+    },
+    {
+        title: '정답률',
+        dataIndex: 'acceptPer',
+        key: 'acceptPer',
+    },
+]
+
 function ProblemList({ list = [] }: Props) {
     const router = useRouter()
-    const vProblemListProps: VProblemProps = {
-        list: list.map((data) => ({
-            ...data,
-            handleRowClick: () => {
-                router.push(`/problem/${data.id}`)
-            },
-            acceptPer: `${getPercentage(data.submission, data.accepted).toFixed(2)}%`,
-        })),
-    }
-    return <VProblemList {...vProblemListProps} />
+
+    const tableData = list.map((problem) => ({
+        ...problem,
+        acceptPer: `${getPercentage(problem.submission, problem.accepted).toFixed(2)}%`,
+    }))
+
+    const handleRowAction = (row: ProblemRow) => ({
+        onClick: () => router.push(`/problem/${row.id}`),
+    })
+
+    return (
+        <BaseTable
+            rowClassName='cursor-pointer'
+            onRow={handleRowAction}
+            rowKey={(row) => row.id}
+            dataSource={tableData}
+            columns={tableColumns}
+            pagination={false}
+        />
+    )
 }
 
 export default ProblemList
